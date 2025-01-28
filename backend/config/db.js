19870a15390827bb0580ee23d34e22dce2ca5af1
atif614 <razaatif20786@gmail.com>
2025-01-28 19:56:50 +0530

    connection with DB

---
+++ backend/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export function ConnectToDB() {
+    mongoose.connect(process.env.MONGO_URI)
+        .then(data => console.log(`Connected to database ${data.connection.host}`))
+        .catch(error => console.log(error))
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import {ConnectToDB} from "./config/db.js";
 dotenv.config({path:"config/config.env"})
-
+ConnectToDB();
 // app.get("/products",getAllProducts);
 // app.get("/product",SingleProduct);
 
