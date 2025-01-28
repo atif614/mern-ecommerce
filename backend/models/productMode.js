ace9366b2614ac5897a2779dd1ad401fdb8fa37a
atif614 <razaatif20786@gmail.com>
2025-01-28 20:41:05 +0530

    created schema for product

---
+++ backend/models/productMode.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Enter Product Name"],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, "Please Enter Product Description"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Please Enter Product Description"],
+        maxLength: [7, "Price cannot exceed 7 digit"]
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    image: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category:{
+        type: String,
+        required: [true, "Please Enter Product Category"]
+    },
+    stock:{
+        type: Number,
+        required: [true, "Please Enter Product Stock"],
+        maxLength: [5, "Quantity cannot exceed 5 digit"],
+        default:1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            rating:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    createdAt: {
+        type:Date,
+        default: Date.now
+    }
+})
+
+export default mongoose.model("'Product",productSchema);
